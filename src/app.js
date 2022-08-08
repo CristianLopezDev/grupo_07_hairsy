01aa892b66424ed9e5abd14e8b6829fcdb83c128
nmansilla32@gmail.com <nmansilla32@gmail.com>
2022-08-08 16:18:30 -0300

    Se crea user.json y product.json + page 404

--- src/app.js
+++ src/app.js
@@ -10,20 +10,16 @@ const productRouter = require('./routes/productRouter');
 
 const userRouter = require('./routes/userRouter');
 
+const methodOverride = require('method-override');
+const { rmSync } = require("fs");
+
+app.use(methodOverride('_method'));
+
 app.set('view engine','ejs')
 
 app.set('views', path.join(__dirname, '/views'))
 
 app.use(express.static('public'));
-
-
-
-
-
-
-
-/*const publicFolderPath = path.resolve(__dirname, "./public");
-app.use (express.static(publicFolderPath));*/
 
 app.use("/public", express.static("public"));
 
@@ -32,6 +28,10 @@ app.use('/',mainRouter)
 app.use('/product', productRouter);
 
 app.use('/user', userRouter);
+
+app.use((req, res, next) => {
+    res.status(404).render('not-found');
+})
 
 app.listen(3000,(req,res) =>{
     console.log("Servidor corriendo en http://localhost:3000")
